7ba9c8a0f990ff9ce4ffc4d7c61250c7e562655e
Jake Sinclair <sinclairs1234@gmail.com>
2026-07-23 23:17:27 +0100

    Fixed links in navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,8 +40,8 @@ const Navbar = () => {
                     className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 bg-[#e6e6ff] transition-all ease-in-out ${open ? "-right-0" : "-right-[100%]"}`}
                 >
                     <Link to="/">Home</Link>
-                    <Link to="/">Trending</Link>
-                    <Link to="/">Most Popular</Link>
+                    <Link to="/posts?sort=trending">Trending</Link>
+                    <Link to="/posts?sort=popular">Most Popular</Link>
                     <Link to="/">About</Link>
                     <Link to="">
                         <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
