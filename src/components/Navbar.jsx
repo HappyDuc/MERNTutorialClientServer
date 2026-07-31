b0f252826605754814d4e2334b0babeecab6d7a3
Jake Sinclair <sinclairs1234@gmail.com>
2026-07-31 16:43:43 +0100

    Corrected about link for desktop.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
                     <Link to="/">Home</Link>
                     <Link to="/posts?sort=trending">Trending</Link>
                     <Link to="/posts?sort=popular">Most Popular</Link>
-                    <Link to="/about">About Jake</Link>
+                    <Link to="/about">About</Link>
                     <Link to="">
                         <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
                             Login
@@ -55,7 +55,7 @@ const Navbar = () => {
                 <Link to="/">Home</Link>
                 <Link to="/posts?sort=trending">Trending</Link>
                 <Link to="/posts?sort=popular">Most Popular</Link>
-                <Link to="/">About</Link>
+                <Link to="/about">About Jake</Link>
                 <Show when="signed-out">
                     <Link to="/login">
                         <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
